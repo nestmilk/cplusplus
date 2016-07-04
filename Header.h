a289b4d3d9dde4e3b4649d896e6c9ea4117bfac4
nestmilk <nestmilk@live.com>
2016-07-04 23:17:10 +0800

    Create Header.h

---
+++ Header.h
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <functional>
+#include <fstream>
+#include <sstream>
+#include <list>
+#include <iterator>
+#include <map>
+#include <set>
+#include <utility>
+#include <memory>
+#include <new>
+#include <cstring>
+#include <stdio.h>
+
+using namespace std;
+using namespace std::placeholders;
